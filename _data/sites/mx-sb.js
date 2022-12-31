66176186759f606ab0484f68b206c6ed1cf781eb
365cent <74791570+365cent@users.noreply.github.com>
2022-12-31 17:55:11 -0500

    Create mx-sb.js

---
+++ _data/sites/mx-sb.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "mx.sb", // optional, falls back to object key
+	description: "Blog on mx.sb",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://mx.sb/"
+	]
+};
